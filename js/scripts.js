73f813f88c51dc523383b21e78f77fdc55909c18
faustlarsen <faustlarsen@gmail.com>
2020-10-20 16:02:44 -0500

    add final project

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $(".clickme").click(function() {
+		$("#hide").toggle();
+		$("#show").toggle();
+  });
+  $(".clickme1").click(function() {
+		$("#show1").toggle();
+		$("#hide1").toggle();
+  });
+  $(".clickme2").click(function() {
+		$("#show2").toggle();
+		$("#hide2").toggle();
+  });
+});
